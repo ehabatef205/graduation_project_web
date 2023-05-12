1d71f4ac170f3600984e99ddc60405744a2036bd
Ziad-Mohammed-Ezzat1712 <201927109@std.sci.cu.edu.eg>
2023-05-12 15:35:49 +0300

    connection with api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 ///  define 
-axios.defaults.baseURL = 'http://localhost:5000/api'
+axios.defaults.baseURL = 'https://spring-bud-cricket-veil.cyclic.app/api'
 axios.defaults.headers.common = {'authorization': localStorage.getItem('Authorization')}
 export default axios;
